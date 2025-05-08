4198484efbe6ba8f67a2c891883bc605fa1b83ea
kody <abdelraouf.gheyat@univ-constantine2.dz>
2025-05-08 14:09:34 +0100

    chore: Update Gradle wrapper and scripts for improved build process
    - Added gradle-wrapper.properties to specify Gradle distribution URL and settings.
    - Introduced gradlew and gradlew.bat scripts for Unix and Windows environments respectively, ensuring compatibility with the new Gradle version.
    - Configured settings.gradle to manage React Native and Expo plugins, enhancing autolinking capabilities and project structure.

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,18 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-       
+      ['module-resolver', {
+        alias: {
+          '@': './src',
+        },
+      }],
+      // Add this plugin to handle Android SDK configuration
+      ['expo-gradle-properties-plugin', {
+        "androidXCore": "1.12.0",
+        "compileSdkVersion": 35,
+        "targetSdkVersion": 33,
+        "buildToolsVersion": "35.0.0"
+      }]
     ]
   };
 };
